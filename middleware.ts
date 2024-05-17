175da6031862945d7ec4a8ce8ddf91d0742c5923
jin123457 <jin_ox@naver.com>
2024-05-17 11:47:25 +0900

    middleware config

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,14 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
-export default async function middlewera(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const session = await getSession();
 
   if (request.nextUrl.pathname === "/profile") {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
