581bd16a032cfc12216c8388d4660b2a0e3628a1
jin123457 <jin_ox@naver.com>
2024-05-17 12:05:43 +0900

    middleware add routes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
+interface IRoots {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: IRoots = {
+  "/": true,
+  "/login": true,
+  "/create-account": true,
+  "/sms": true,
+};
+
 export async function middleware(request: NextRequest) {
   const session = await getSession();
+  const exits = publicOnlyUrls[request.nextUrl.pathname];
 
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
+  if (!session.id) {
+    if (!exits) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exits) {
+      return NextResponse.redirect(new URL("/produts", request.url));
+    }
   }
 }
 
